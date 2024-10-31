9df8de0bba1b2dd7d73d3ade6c77326b1ff83fd6
krishna4mern <krishna4job4mern@gmail.com>
2024-10-31 16:26:49 +0530

    login and register done

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,5 +1,6 @@
 const UserModel = require("../models/User");
 const bcrypt = require("bcryptjs");
+const {generateToken}  = require("../utils/generateToke")
 
 module.exports.registerUser = async (req, res) => {
   try {
@@ -23,4 +24,24 @@ module.exports.registerUser = async (req, res) => {
   }
 };
 
+
+module.exports.loginUser = async (req , res) => {
+  try {
+    const {email , password} = req.body;
+    const user = await UserModel.findOne({email: email});
+    if(!user) res.status(404).send({status:false  , message:"Don't have an account "});
+    const isMatchPassword = await bcrypt.compare(password, user.password);
+    if(!isMatchPassword) {
+      return res.status(400).send({status:false  , message:"Incorrect password"});
+    }
+    else{
+      let token = generateToken(user);
+      res.cookie("token", token);
+      res.status(200).send({status:true  , message:"Login Successful", user});
+    }
+
+  }catch(err){
+    console.log(`Error comes from /login and error is ${err.message}`);
+  }
+}
 
--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
+const cookieParser = require('cookie-parser');
 const userRouter = require("./routes/userRouter");
 
 require("./db-connection/db");
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("server is running");
